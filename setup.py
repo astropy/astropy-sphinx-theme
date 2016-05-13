25b2786787276ac6a7a6977dd0afa5ec14f9dfca
Stuart Mumford <stuart@cadair.com>
2016-05-13 16:07:49 +0100

    Initital commit of installable theme

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+from astropy_sphinx_theme import __version__
+
+setup(
+    name="sphinx-astropy-theme",
+    version=__version__,
+    use_2to3=False,
+    description="The sphinx theme for Astropy.",
+    long_description="The documentation theme for the Astropy project and affiliated packages.",
+    author="The Astropy Developers",
+    author_email="astropy@scipy.org",
+    install_requires=[
+        "setuptools",
+    ],
+    packages=['sphinx_astropy_theme'],
+    include_package_data=True,
+)
