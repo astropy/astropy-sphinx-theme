4a5dc388471d3d847e7bbb95da8a56ccbd75db65
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-01-26 11:39:11 +0100

    Convert README to rst and use as long description

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup, find_packages
 
 from astropy_sphinx_theme import __version__
 
+with open("README.rst") as f:
+    LONG_DESCRIPTION = f.read()
+
 setup(
     name="astropy-sphinx-theme",
     version=__version__,
+    url="https://github.com/astropy/astropy-sphinx-theme",
     use_2to3=False,
-    description="The sphinx theme for Astropy.",
-    long_description="The documentation theme for the Astropy project and affiliated packages.",
+    description="The sphinx theme for Astropy and affiliated packages.",
+    long_description=LONG_DESCRIPTION,
     author="The Astropy Developers",
     author_email="astropy.team@gmail.com",
     install_requires=["setuptools"],
@@ -17,4 +21,4 @@ setup(
         'sphinx.html_themes': [
             'bootstrap-astropy = astropy_sphinx_theme',
         ]
-    },)
+    })
