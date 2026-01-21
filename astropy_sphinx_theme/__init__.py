c5a6d4752e1aa3ab3d8120bb28522290c7965ef6
Stuart Mumford <stuart@cadair.com>
2026-01-21 14:40:19 +0000

    Deprecate old function

---
+++ astropy_sphinx_theme/__init__.py
@@ -0,0 +1,12 @@
+import warnings
+
+from .bootstrap_astropy import get_html_theme_path as _old_theme_path
+
+
+def get_html_theme_path():
+    warnings.warn(
+        "astropy_sphinx_theme.get_html_theme_path is deprecated, you shouldn't"
+        " need to manually specify the theme path as the theme is registered with sphinx",
+        DeprecationWarning,
+    )
+    return _old_theme_path()
