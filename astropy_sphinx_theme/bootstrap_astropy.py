9ec386996b83a7c68c0fc09ae79b09069c24bc3e
Stuart Mumford <stuart@cadair.com>
2026-01-21 14:40:19 +0000

    Deprecate old function

--- astropy_sphinx_theme/bootstrap_astropy.py
+++ astropy_sphinx_theme/bootstrap_astropy.py
@@ -3,6 +3,11 @@
 from pathlib import Path
 
 
+# This is kept for backwards compatibility reasons (see __init__.py)
+def get_html_theme_path():
+    return [str(Path(__file__).resolve().parent / "bootstrap-astropy")]
+
+
 def setup(app):
     app.add_html_theme(
         "bootstrap-astropy",
