6a73c6c3f4bd87e9f28232447f2767434200d020
Stuart Mumford <stuart@cadair.com>
2026-01-21 13:27:48 +0000

    Add a doc build

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Astropy Sphinx Theme"
+copyright = "2026, The Astropy Developers"
+author = "The Astropy Developers"
+release = "2.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "bootstrap-astropy"
+# html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,13 @@
+Astropy Sphinx Theme documentation
+==================================
+
+These documentation pages are partly to document the astropy sphinx themes, but also to provide a test documentation build where theming choices can be seen.
+
+This package provides the ``bootstrap-astropy`` theme.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   colors
+
